07c6a7a1c0ff063c08170be63b65ba56cb1d7927
sturdivant <sturdivantayla@gmail.com>
2025-09-17 13:33:39 -0500

    resubmit

--- I2 - Math/jasmine/src/main.js
+++ I2 - Math/jasmine/src/main.js
@@ -8,9 +8,9 @@ let workdays = Math.floor(budget / dayRate);
 
 // Task 3: Calculate the Discounted Rate for Large Projects
 let billable = 22;
-let monthlyDiscount = 0.2;
+let monthlyDiscount = (billable * dayRate) * 0.2;
 let fullMonths = Math.floor(35 / billable);
 let remainingDays = 35 - billable;
-let discountedMonthlyRate = dayRate * billable * (1 - monthlyDiscount);
+let discountedMonthlyRate = billable * dayRate - monthlyDiscount;
 let remainingCost = remainingDays * dayRate;
 let totalProjectCost = Math.ceil(discountedMonthlyRate + remainingCost);
---
+++ I3 - Strings/jasmine/src/main.js
@@ -0,0 +1,11 @@
+// Task 1: Get the First Letter of a Line
+let line = 'Stands so high'
+let frontDoorLetter = line[0];
+
+// Task 2: Capitalize the Front Door Password
+
+
+// Task 3: Get the Last Letter of a Line
+
+
+// Task 4: Make a Polite Back Door Password
