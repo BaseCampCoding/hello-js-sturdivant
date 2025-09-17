30704a908c008de5a99ca9fcd913d8d52f999871
sturdivant <sturdivantayla@gmail.com>
2025-09-17 14:41:00 -0500

    submission for 4-Boolean

---
+++ 4-Boolean/jasmine/src/main.js
@@ -0,0 +1,16 @@
+let knightIsAwake = true;
+let archerIsAwake = false;
+let prisonerIsAwake = true;
+let petDogIsPresent = true;
+
+// Task 1: Fast Attack Availability
+let canExectuteFastAttack = false;
+
+// Task 2: Spy Availability
+let canSpy = true;
+
+// Task 3: Signal Prisoner Availability
+let canSignalPrisoner = true;
+
+// Task 4: Free Prisoner 
+let canFreePrisoner = true;
--- I3 - Strings/jasmine/src/main.js
+++ I3 - Strings/jasmine/src/main.js
@@ -3,9 +3,14 @@ let line = 'Stands so high'
 let frontDoorLetter = line[0];
 
 // Task 2: Capitalize the Front Door Password
-
+let frontDoorWord = 'SHIRE';
+let frontDoorPassword = frontDoorWord[0].toUpperCase() + frontDoorWord.slice(1,5).toLowerCase();
 
 // Task 3: Get the Last Letter of a Line
-
+lineWithWhitespace = 'Reins and harness ';
+let removeWhitespace = lineWithWhitespace.trim();
+let backDoorLetter = removeWhitespace[removeWhitespace.length - 1]
 
 // Task 4: Make a Polite Back Door Password
+let backDoorWord = 'Horse';
+let backDoorPassword = backDoorWord + ', please';
