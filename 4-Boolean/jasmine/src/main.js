97220bf8604f74d9d37b24980a615a0c6c5fe161
sturdivant <sturdivantayla@gmail.com>
2025-09-18 09:52:00 -0500

    resubmit

--- 4-Boolean/jasmine/src/main.js
+++ 4-Boolean/jasmine/src/main.js
@@ -5,12 +5,27 @@ let petDogIsPresent = true;
 
 // Task 1: Fast Attack Availability
 let canExectuteFastAttack = !knightIsAwake;
+// False
+if (knightIsAwake == false) {
+    canExectuteFastAttack = true
+}
+else {
+    canExectuteFastAttack = false
+}
+console.log('Fast Attack: ' + canExectuteFastAttack);
 
 // Task 2: Spy Availability
 let canSpy = knightIsAwake || archerIsAwake || prisonerIsAwake;
+console.log('Spy: ' + canSpy);
+// True
 
 // Task 3: Signal Prisoner Availability
-let canSignalPrisoner = prisonerIsAwake && archerIsAwake;
+let canSignalPrisoner = prisonerIsAwake && !archerIsAwake;
+console.log('Signal Prisoner: ' + canSignalPrisoner);
+// True
 
 // Task 4: Free Prisoner 
-let canFreePrisoner = petDogIsPresent && !archerIsAwake || !petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake;
+let canFreePrisoner = (petDogIsPresent && !archerIsAwake) ||
+    (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
+console.log('Free Prisoner: ' + canFreePrisoner);
+// True
---
+++ 6-Conditionals/jasmine/src/main.js
@@ -0,0 +1,22 @@
+// Task 1: Determine if you need a driver's license
+let kind = 'car';
+let needsLicenseResult = true;
+
+// Task 2: Choose between two potential vehicles to buy
+let option1 = 'Toyota';
+let option2 = 'Ford';
+let chooseVehicleResult = Array.prototype.sort + "is clearly the better choice.";
+
+// Task 3: Calculate an estimation for the price of a used vehicle
+let originalPrice = 100;
+let age = 5;
+let calculateResellPrice
+    if (age < 3) {
+        originalPrice = originalPrice * 0.8;
+    }
+    else if (age > 10) {
+        originalPrice = originalPrice * 0.5;
+    }
+    else (age >= 3 && age < 10) {
+        originalPrice = 0.7
+    };
