eb313e0b94f8acf8d64d401670ef00ef36722723
Brittany Cohen <88344074+b-cohen@users.noreply.github.com>
2025-09-19 11:42:25 -0500

    Update main.js

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -1,24 +1,4 @@
-let ratePerHour = 60;
 
-let dayRate = ratePerHour * 8;
-
-let budget = 1000;
-
-let workdays = Math.floor(budget / dayRate);
-
-let billable = 0;
-
-let fullMonths = 1;
-
-let remainingDays = 13
-
-let monthlyDiscount = 0.2;
-
-let discountedMonthlyRate = dayRate * (22 * (1 - monthlyDiscount));
-
-let remainingCost = remainingDays * dayRate;
-
-let totalProjectCost = (fullMonths * discountedMonthlyRate) + remainingCost;
 
 
 
