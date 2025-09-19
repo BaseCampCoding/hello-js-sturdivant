bfda5b47fe56064175e33e1d94a12a830f0b8ce6
Brittany Cohen <88344074+b-cohen@users.noreply.github.com>
2025-09-19 11:46:20 -0500

    Remove variables related to oven timing

--- 1-Basics/jasmine/src/main.js
+++ 1-Basics/jasmine/src/main.js
@@ -1,11 +1,1 @@
-const EXPECTED_MINUTES_IN_OVEN = 40;
 
-let actualMinutesInOven = 25;
-
-let remainingMinutesInOven = EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
-
-let preparationTimeInMinutes = 6;
-
-let numberOfLayers = 3;
-
-let totalTimeInMinutes = preparationTimeInMinutes + actualMinutesInOven;
