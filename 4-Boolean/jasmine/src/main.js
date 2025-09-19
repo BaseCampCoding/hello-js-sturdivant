4efe4ccb77c13653bb58430ff5e64242b4fc1f7e
Brittany Cohen <88344074+b-cohen@users.noreply.github.com>
2025-09-19 11:43:17 -0500

    Remove boolean variables and related logic

--- 4-Boolean/jasmine/src/main.js
+++ 4-Boolean/jasmine/src/main.js
@@ -1,12 +1,1 @@
-let knightIsAwake = true;
-let archerIsAwake = false;
-let prisonerIsAwake = true;
-let petDogIsPresent = true;
 
-let canExecuteFastAttack = !knightIsAwake;
-
-let canSpy = knightIsAwake || archerIsAwake || prisonerIsAwake;
-
-let canSignalPrisoner = prisonerIsAwake && !archerIsAwake;
-
-let canFreePrisoner = (petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
