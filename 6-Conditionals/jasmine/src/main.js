fdcb519db60d5868eb926368e9767892adcb9b7b
Brittany Cohen <88344074+b-cohen@users.noreply.github.com>
2025-09-19 11:44:09 -0500

    Remove vehicle-related tasks from main.js

--- 6-Conditionals/jasmine/src/main.js
+++ 6-Conditionals/jasmine/src/main.js
@@ -1,40 +1,4 @@
-//Task 1
-let kind = "car"
 
-let needsLicenseResult;
-
-if (kind === "car" || kind === "truck" || kind === "motorcycle") {
-  needsLicenseResult = true;
-} else {
-  needsLicenseResult = false;
-}
-
-// Task 2
-const option1 = "Hyundai";
-
-const option2 = "Jeep";
-
-let chosenVehicle;
-if (option1.localeCompare(option2) < 0) {
-  chosenVehicle = option1;
-} else {
-  chosenVehicle = option2;
-}
-
-let chooseVehicleResult = `${chosenVehicle} is clearly the better choice.`;
-
-// Task 3
-let originalPrice = 25000;
-let age = 5;
-let calculateResellPriceResult;
-
-if (age < 3) {
-  calculateResellPriceResult = originalPrice * 0.80;
-} else if (age > 10) {
-  calculateResellPriceResult = originalPrice * 0.50;
-} else {
-  calculateResellPriceResult = originalPrice * 0.70;
-}
 
 
 
